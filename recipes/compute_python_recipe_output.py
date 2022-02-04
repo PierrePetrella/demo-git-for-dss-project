279a991d918120db0c9f06537b6e8a7e0d27b3ed
pierrepetrella <pierre.petrella@dataiku.com>
2022-02-04 11:13:11 +0000

    Saved recipe: DKU_HAIKU_STARTER_1.compute_python_recipe_output

--- recipes/compute_python_recipe_output.py
+++ recipes/compute_python_recipe_output.py
@@ -12,6 +12,7 @@ Orders_enriched_prepared_df = Orders_enriched_prepared.get_dataframe()
 python_recipe_output_df = Orders_enriched_prepared_df.describe() # For this sample code, simply copy input to output
 
 
+
 # Write recipe outputs
 python_recipe_output = dataiku.Dataset("python_recipe_output")
 python_recipe_output.write_with_schema(python_recipe_output_df)
