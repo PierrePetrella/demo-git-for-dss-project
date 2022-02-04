a0aa90bf79623275e958c653ae2203b7ac4d4044
pierrepetrella <pierre.petrella@dataiku.com>
2022-02-04 11:29:03 +0000

    edit files

--- lib/python/lib_import_same_repo_empl/my_deep_function.py
+++ lib/python/lib_import_same_repo_empl/my_deep_function.py
@@ -1,2 +1,2 @@
 def do_something2():
-    print ("Printing from an library imported from a subset of a git repo to this project's code library")
+    print ("Printing from an library imported from a subset of a git repo to this project's code library 2")
