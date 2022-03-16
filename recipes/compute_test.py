299b22d34cf2d9112b21d95264e594ae9dea1af6
pierrepetrella <pierre.petrella@dataiku.com>
2022-03-16 10:44:16 +0000

    Saved recipe: DKU_HAIKU_STARTER_1.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -5,11 +5,8 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-from lib_import_empl import my_function
-from test import run_tests
-
-
-run_tests.run_tests()
+from remote_library.test.run_tests import run_tests
+run_tests()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
