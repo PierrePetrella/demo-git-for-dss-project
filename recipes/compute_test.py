923007c9a4ccaebd00dc7b91444065538e57cbc7
pierrepetrella <pierre.petrella@dataiku.com>
2022-03-16 10:15:06 +0000

    Saved recipe: DKU_HAIKU_STARTER_1.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -3,18 +3,13 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
-# Read recipe inputs
-Orders_enriched = dataiku.Dataset("Orders_enriched")
-Orders_enriched_df = Orders_enriched.get_dataframe()
+
+import 
 
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-test_df = Orders_enriched_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
 test = dataiku.Dataset("test")
-test.write_with_schema(test_df)
+test.write_with_schema(pd.DataFrame())
