fa27e84f4b624c819f05ac305589bf4eb17b6bdd
pierrepetrella <pierre.petrella@dataiku.com>
2022-03-16 10:33:17 +0000

    Recipe DKU_HAIKU_STARTER_1.compute_test saved through notebook notebook editor for compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -1,15 +1,15 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import pytest
 
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 from test.run_tests import run_tests
+run_tests()
 
-
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 test = dataiku.Dataset("test")
 test.write_with_schema(pd.DataFrame())
