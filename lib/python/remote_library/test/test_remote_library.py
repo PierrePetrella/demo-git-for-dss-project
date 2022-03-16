13c7d6ae7a787cea58b7d4b1cf9700a955b7910a
pierrepetrella <pierre.petrella@dataiku.com>
2022-03-16 11:00:16 +0000

    edit files

--- lib/python/remote_library/test/test_remote_library.py
+++ lib/python/remote_library/test/test_remote_library.py
@@ -2,6 +2,8 @@
 # This is a test file intended to be used with pytest
 # pytest automatically runs all the function starting with "test_"
 # see https://docs.pytest.org for more information
+import pytest
+
 
 from remote_library import my_function
 
