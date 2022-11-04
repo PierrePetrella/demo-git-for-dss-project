6ac7fed150e69e89fe2faf8254a2d8a3a84e9025
pierrepetrella <pierrepetrella>
2022-11-04 15:58:41 +0000

    Git references update for GIT_INTEGRATION_DEMO

--- lib/python/remote_library/my_function.py
+++ lib/python/remote_library/my_function.py
@@ -1,3 +1,3 @@
 def do_something():
-    print ("Printing from an library imported from git to this project's code library - v2")
+    print ("Printing from an library imported from git to this project's code library - Update")
     return 5
